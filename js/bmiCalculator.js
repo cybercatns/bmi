a00ba8c7da57f4d1c4d457f03791ed694666c79c
cybercatns <contactsahan@gmail.com>
2020-10-26 01:01:54 +0530

    updated to jquery

--- js/bmiCalculator.js
+++ js/bmiCalculator.js
@@ -1,42 +1,48 @@
-bmiCalc(weight, height);
+$(".calcbtn").on("click", function() {
 
-function bmiCalc(weight, height){
+    var weight = $("#weightInput").val();
+    var height = $("#heightInput").val();
+    
+    console.log("weightInput is: "+ weight);
+    console.log("heightInput is: "+ height);
+
     var heightMeters = height / 100;
     var bmi = weight / (heightMeters*heightMeters);
 
-    var arr = document.getElementsByClassName("alert");
+    $(".alert").css("border", "0rem");
 
-    for(var i=0; i < arr.length; i++){
-        document.getElementsByClassName("alert")[i].style.border="0rem";
-    }
+    if(weight == "" || height == ""){
+        $("#yourbmi").html("Fail");
+    } else{
+        if(bmi < 18.5){
+            console.log("inside ash: "+bmi);
+            $("#yourbmi").html(bmi.toFixed(1));
+            $("#yourbmi").css("color", "#707070");
+            $(".Underweight").css("border", "dotted");
+            $(".Underweight").css("borderWidth", "0.5rem");
+    
+        } else if(bmi >= 18.5 && bmi < 24.9){
+            console.log("inside green: "+bmi);
+            $("#yourbmi").html(bmi.toFixed(1));
+            $("#yourbmi").css("color", "green");
+            $(".Normal").css("border", "dotted");
+            $(".Normal").css("borderWidth", "0.5rem");
+    
+        } else if(bmi >= 25 && bmi < 29.9){
+            console.log("inside yellow: "+bmi);
+            $("#yourbmi").html(bmi.toFixed(1));
+            $("#yourbmi").css("color", "yellow");
+            $(".Overweight").css("border", "dotted");
+            $(".Overweight").css("borderWidth", "0.5rem");
+        } else {
+            console.log("inside red: "+bmi);
+            $("#yourbmi").html(bmi.toFixed(1));
+            $("#yourbmi").css("color", "red");
+            $(".Obesity").css("border", "dotted");
+            $(".Obesity").css("borderWidth", "0.5rem");
+        }
+    }    
+  });
 
-    if(bmi < 18.5){
-        console.log("inside ash: "+bmi);
-        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
-        document.getElementById("yourbmi").style.color = "#707070";
-        document.getElementsByClassName("Underweight")[0].style.border = "dotted";
-        document.getElementsByClassName("Underweight")[0].style.borderWidth = "0.5rem";
-    } else if(bmi >= 18.5 && bmi < 24.9){
-        console.log("inside green: "+bmi);
-        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
-        document.getElementById("yourbmi").style.color = "green";
-        document.getElementsByClassName("Normal")[0].style.border = "dotted";
-        document.getElementsByClassName("Normal")[0].style.borderWidth = "0.5rem";
-    } else if(bmi >= 25 && bmi < 29.9){
-        console.log("inside yellow: "+bmi);
-        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
-        document.getElementById("yourbmi").style.color = "yellow";
-        document.getElementsByClassName("Overweight")[0].style.border = "dotted";
-        document.getElementsByClassName("Overweight")[0].style.borderWidth = "0.5rem";
-    } else {
-        console.log("inside red: "+bmi);
-        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
-        document.getElementById("yourbmi").style.color = "red";
-        document.getElementsByClassName("Obesity")[0].style.border = "dotted";
-        document.getElementsByClassName("Obesity")[0].style.borderWidth = "0.5rem";
-    }
-    
-    
-    // document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
-}
+
 
