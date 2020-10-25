0faec41c13b33965b8ce1f3660be390a0fdac056
cybercatns <contactsahan@gmail.com>
2020-10-25 15:59:31 +0530

    version 1.10

--- js/bmiCalculator.js
+++ js/bmiCalculator.js
@@ -3,7 +3,27 @@ bmiCalc(weight, height);
 function bmiCalc(weight, height){
     var heightMeters = height / 100;
     var bmi = weight / (heightMeters*heightMeters);
-    console.log(bmi);
-    document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
+    // console.log(bmi);
+
+    if(bmi < 18.5){
+        console.log("inside ash: "+bmi);
+        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
+        document.getElementById("yourbmi").style.color = "#707070";
+    } else if(bmi >= 18.5 && bmi < 24.9){
+        console.log("inside green: "+bmi);
+        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
+        document.getElementById("yourbmi").style.color = "green";
+    } else if(bmi >= 25 && bmi < 29.9){
+        console.log("inside yellow: "+bmi);
+        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
+        document.getElementById("yourbmi").style.color = "yellow";
+    } else {
+        console.log("inside red: "+bmi);
+        document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
+        document.getElementById("yourbmi").style.color = "red";
+    }
+    
+    
+    // document.getElementById("yourbmi").innerHTML = bmi.toFixed(1);
 }
 
